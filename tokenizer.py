b6200940eaa2dc15758ced79f0ac13e74c4475e7
김영선 <withbbang@gmail.com>
2024-06-20 19:25:23 +0900

    Feat: 변환 함수 추가

--- tokenizer.py
+++ tokenizer.py
@@ -14,3 +14,29 @@ max_token_length = 64
 
 # 3. 토크나이저 불러오기
 tokenizer = AutoTokenizer.from_pretrained(model_ckpt)
+
+def convert_examples_to_features(examples):
+    ###########################################################################
+    # with 쓰는 옛날 방식
+    # input_encodings = tokenizer(examples['en'], 
+    #                             max_length=max_token_length, truncation=True)
+    
+    # Setup the tokenizer for targets
+    # with tokenizer.as_target_tokenizer():
+    # target_encodings = tokenizer(text_target=examples['ko'], 
+    #                             max_length=max_token_length, truncation=True)
+    #
+    #
+    # return {
+    #     "input_ids": input_encodings["input_ids"],
+    #     "attention_mask": input_encodings["attention_mask"],
+    #     "labels": target_encodings["input_ids"]
+    # }
+    
+    # 그런데 이렇게 하면 인풋하고 한번에 처리 가능함.
+    model_inputs = tokenizer(examples['en'],
+                             text_target=examples['ko'], 
+                             max_length=max_token_length, truncation=True)
+    
+    return model_inputs
+
